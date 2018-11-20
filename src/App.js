6ad2d8244b9efddbdb9dc81e6ce1faee8b327357
aehaq <azfarehaq@gmail.com>
2018-11-19 16:27:21 -0800

    src file laid out

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Filters from "./components/Filters";
+import Projects from "./components/Projects";
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    filter: "Featured",
+    expanded: false
+  };
+
   render() {
     return (
       <Wrapper>
-        
+        <Header
+          expanded={this.state.expanded}
+        />
+        <Filters />
+        <Projects 
+          filter={this.state.filter}
+        />
       </Wrapper>
     );
   }

7e7540dab1a06f86fc5c4c131c82f2734b35bf51
aehaq <azfarehaq@gmail.com>
2019-01-09 14:23:27 -0800

    dropdown example functioning

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,46 @@
 import React, { Component } from "react";
 import "./Projects.css";
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
 
 class Projects extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          dropdownOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        })
+    }
+
     render() {
         return(
             <div>
                 <div className="filters">
-                    
+                <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                    <DropdownToggle caret>
+                        Button Dropdown
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem header>JavaScript</DropdownItem>
+                        <DropdownItem>Action</DropdownItem>
+                        <DropdownItem>Action</DropdownItem>
+                        <DropdownItem>Action</DropdownItem>
+                        <DropdownItem>Action</DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem header>Python</DropdownItem>
+                        <DropdownItem>Action</DropdownItem>
+                        <DropdownItem>Action</DropdownItem>
+                        <DropdownItem>Action</DropdownItem>
+                        <DropdownItem>Action</DropdownItem>
+                    </DropdownMenu>
+                </ButtonDropdown>
                     
                 </div>
                 <div className="cards">

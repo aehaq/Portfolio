ae34f08383da3546f4456126e10a7ce2651b3548
aehaq <azfarehaq@gmail.com>
2019-01-15 09:45:40 -0800

    header dropdown button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import { Collapse, Button } from 'reactstrap';
 import "./Header.css";
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { collapse: false }
+    }
+
+    toggle() {
+        this.setState({ collapse: !this.state.collapse })
+    }
+
     render() {
         return(
             <header className="header">
@@ -10,6 +22,14 @@ class Header extends Component {
                 <a>GitHub</a>
                 <a>Resume</a>
                 <a>Contact</a>
+                <Button color="primary" onClick={this.toggle} >Dropdown</Button>
+                <Collapse isOpen={this.state.collapse}>
+                <div>
+                    <h1>
+                        HELLO WORLD!
+                    </h1>
+                </div>
+                </Collapse>
             </header>
         )
     }

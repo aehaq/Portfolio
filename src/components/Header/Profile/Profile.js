c2cda0f9764169a91124e6db999f72cbf51a58de
aehaq <azfarehaq@gmail.com>
2019-01-25 13:50:30 -0800

    Style tweaking

--- src/components/Header/Profile/Profile.js
+++ src/components/Header/Profile/Profile.js
@@ -13,17 +13,19 @@ class Profile extends Component {
         return(
             <div>
                 <Container>
-                    <Row>
-                        <Col md="4">
-                            <img src={profilePic} className="prof-pic" />
+                    <Row className="pt-3">
+                        <Col lg="4">
+                            <Container className="img-container">
+                                <img src={profilePic} className="prof-pic p-0" />
+                            </Container>
                         </Col>
-                        <Col md="4">
+                        <Col lg="4">
                             <p className="m-2 text-secondary"> As a Full Stack Developer, I have built web applications from the ground up requiring Node runtime-environments, relational and non-relational databases, RESTful API architecture and front-end JavaScript frameworks, such as React and Handlebars.
                                 <br />
                                 I am eager to build new features and applications as I enjoy untangling complex logical processes and pride myself on my ability to foresee issues and exercise prevention rather than remedy when writing code. Learning new frameworks and more efficient ways to create usable and useful tools for people to enjoy is my forte.
                             </p>
                         </Col>
-                        <Col md="4">
+                        <Col lg="4">
                             
                         </Col>
                     </Row>

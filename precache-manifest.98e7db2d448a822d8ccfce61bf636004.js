9191c2dbf2c0be54bbf8369e2c2383361787bb4a
aehaq <azfarehaq@gmail.com>
2019-01-29 09:55:48 -0800

    Updates

---
+++ precache-manifest.98e7db2d448a822d8ccfce61bf636004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "733b3ffb2e4fb357ba6b",
+    "url": "/Portfolio/static/js/main.733b3ffb.chunk.js"
+  },
+  {
+    "revision": "7dcab0d01621ce478ca2",
+    "url": "/Portfolio/static/js/1.7dcab0d0.chunk.js"
+  },
+  {
+    "revision": "733b3ffb2e4fb357ba6b",
+    "url": "/Portfolio/static/css/main.40f45521.chunk.css"
+  },
+  {
+    "revision": "0cfea99ecc3b5a4f8367999e1a2e4bd1",
+    "url": "/Portfolio/index.html"
+  }
+];
